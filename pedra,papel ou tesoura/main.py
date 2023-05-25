3059c1bc1ce0ed7f329e5c140e902b886e57d534
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2023-05-25 09:21:05 -0300

    Update main.py

--- pedra,papel ou tesoura/main.py
+++ pedra,papel ou tesoura/main.py
@@ -1,36 +1,61 @@
 from pickle import TRUE
 from secrets import choice
-
+import threading 
+import asyncio
+import time
 
 
 import random
-choices = ['rock','papper','scissors']
 
 
-playerchoice = None
-
-computer = random.choice(choices)
-while(playerchoice not in choices):
-    playerchoice = input("rock papper or scissor ? ").lower()
-        
-    print(computer)
-    if playerchoice == computer:
-            print("empate")
-
-    if playerchoice == "rock" :
-        if computer == "papper" :
-            print("computer win")
-        elif computer == "scissors":
-            print("player win")
+async def computerChoice(computer):
+    choices = ['pedra','papel','tesoura']
     
-    if playerchoice == "papper" :
-        if computer == "scissors" :
-            print("computer win")
-        elif computer == "rock":
-            print("player win")
-    if playerchoice == "scissors" :
-        if computer == "rock" :
-            print("computer win")
-        elif computer == "papper":
-            print("player win")
-                       
+    computer = await random.choice(choices)
+
+    print(computer)
+    
+
+async def game(rounds):
+    computer1 = ""
+
+    computer2 = ""
+
+    i = 0
+
+
+    pointsPlayer = 0
+    pointsComputer = 0
+    while i<=rounds: 
+        thread1 =  threading.Thread(target=computerChoice, args=(computer1,))
+        thread1.start()
+        thread1.join()
+        thread2 =   threading.Thread(target=computerChoice, args=(computer2,))
+        thread2.start()
+        thread2.join()  
+        print(computer1)
+        print(computer2)
+        if computer1 == computer2:
+                print("empate")
+        if computer2 == "pedra" :
+            if computer1  == "papel" :
+                print("computador2 ganhou")
+                pointsComputer+=1
+            elif computer1 == "tesoura":
+                print("computador1 ganhou")
+                pointsPlayer+=1
+        if computer2 == "papel" :
+            if computer1 == "tesoura" :
+                print("computador1 ganhou")
+            elif computer1 == "pedra":
+                print("computador2 ganhou")
+        if computer2 == "tesoura" :
+            if computer1 == "pedra" :
+                print("computador1 ganhou")
+            elif computer1 == "papper":
+                print("computador2 ganhou")
+        i+=1
+
+rounds = int(input("digite o numero de rodadas "))
+asyncio.run(game(rounds))
+
